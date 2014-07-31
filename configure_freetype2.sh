50eef8677a935c6a3c7820afa209211abe088457
Xavier Hallade <xavier.hallade@intel.com>
2014-07-31 12:01:09 +0200

    added x86 support (call NDK_ABI=x86 ./configure_make_everything.sh to generate x86 binaries)

--- configure_freetype2.sh
+++ configure_freetype2.sh
@@ -4,12 +4,20 @@ pushd `dirname $0`
 
 pushd freetype2
 
+case $TARGET_ARCH in
+  x86)
+  ;;
+  arm)
+  EXTRA_CFLAGS="$EXTRA_CFLAGS -mcpu=cortex-a8 -marm -mfloat-abi=softfp -mfpu=neon"
+  ;;
+esac
+
 ./autogen.sh
 
 ./configure \
     CC="$CC" \
     LD="$LD" \
-    CFLAGS="-std=gnu99 -mcpu=cortex-a8 -marm -mfloat-abi=softfp -mfpu=neon -fPIE -pie" \
+    CFLAGS="-std=gnu99 -fPIE -pie $EXTRA_CFLAGS" \
     --host=$HOST \
     --with-sysroot="$NDK_SYSROOT" \
     --enable-static \
